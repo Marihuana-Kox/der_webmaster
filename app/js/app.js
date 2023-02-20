4b8f3a4d6c87a7a717063142467c616c18e9cdac
Marihuana-Kox <yakuhs@yandex.ru>
2023-02-20 21:51:36 +0300

    20.02.2023

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,20 @@
 $(function(){
     // $('body').hide();
+    function size_window() {
+        if ($(window).width() < 1200 ){
+            $('.dextop__meny').addClass('burger__meny')
+            $('.burger__meny').removeClass('dextop__meny')
+        }else{
+            $('.burger__meny').addClass('dextop__meny')
+            $('.dextop__meny').removeClass('burger__meny')
+        }
+    }
+    
+    $('.burger').toggle( function() {
+        $('.burger__meny').css('display', 'block');
+    }, function() {
+        $('.burger__meny').css('display', 'none');
+    })
+
+    $(window).resize(size_window)
 })
